f154eb88dae5d49beec2c80ec80081bb6ce931d4
Kanimozhi-RAVI <mozhi9605@gmail.com>
2025-06-05 17:30:26 +0530

    first commit

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-// 🛠 Create Axios instance
 const api = axios.create({
   baseURL: 'https://hastin-container.com/staging/app',
 });
 
-// 🛡 Add interceptor to attach BslogiKey token
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('authToken'); // store this in localStorage earlier
+  const token = localStorage.getItem('authToken');
   if (token) {
     config.headers['Authorization'] = `BslogiKey ${token}`;
   }
