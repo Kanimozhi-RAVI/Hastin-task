79b7530025ee716479562413640f1948426a247d
Kanimozhi-RAVI <mozhi9605@gmail.com>
2025-06-19 10:09:45 +0530

    complete the status

--- src/Components/Saga/VendorSaga.jsx
+++ src/Components/Saga/VendorSaga.jsx
@@ -355,7 +355,7 @@ function* putcontactlist(action) {
       email: action.payload.email,
       id:action.payload.id,
       mobileNo: Number(action.payload.mobileNo),
-      isDefault: action.payload.isDefault,
+      isDefault: action.payload.isDefault === "YES" ? true : false,
       vendorId: action.payload.vendorId,
       createdBy: action.payload.createdBy,
     };
